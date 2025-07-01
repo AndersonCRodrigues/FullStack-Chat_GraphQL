bdefa048d1891e1038ca38ba9d487dd636fe4afa
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2025-07-01 14:27:49 -0300

    feat: config graphQL and modules

---
+++ backend/src/main.ts
@@ -0,0 +1,37 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
+import { DEFAULT_SERVER_PORT } from './common/constants/app.constants';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const logger = new Logger('Bootstrap');
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      disableErrorMessages: process.env.NODE_ENV === 'production',
+    }),
+  );
+
+  const port = configService.get<number>('PORT') || DEFAULT_SERVER_PORT;
+  await app.listen(port);
+  logger.log(
+    `🚀 GraphQL API com SQLite e Chat está rodando em: http://localhost:${port}/graphql`,
+  );
+  logger.log(`Ambiente: ${process.env.NODE_ENV || 'development'}`);
+}
+bootstrap().catch((error) => {
+  console.error('Error during bootstrap:', error);
+  process.exit(1);
+});
