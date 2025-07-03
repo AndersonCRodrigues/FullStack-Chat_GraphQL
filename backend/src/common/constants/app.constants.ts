e174b9fcb479bc7e9111998ce6125461e912acbf
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2025-07-03 16:22:37 -0300

    feat: constants

---
+++ backend/src/common/constants/app.constants.ts
@@ -0,0 +1,4 @@
+export const REFRESH_TOKEN_COOKIE_NAME = 'refreshToken';
+export const JWT_ACCESS_TOKEN_EXPIRATION = '15m';
+export const JWT_REFRESH_TOKEN_EXPIRATION = '7d';
+export const DEFAULT_SERVER_PORT = 4000;
