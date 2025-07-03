837d09691febdc3c8595649dec057de19199ad4d
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2025-07-03 16:25:11 -0300

    feat: chat constant

---
+++ backend/src/chat/constants/chat.constants.ts
@@ -0,0 +1,3 @@
+export const MESSAGE_TTL_MINUTES: number = 15;
+export const MESSAGE_ADDED_EVENT_BASE: string = 'messageAdded_';
+export const CHAT_ROOMS: string[] = ['general', 'developers', 'support'];
