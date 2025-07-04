39c65724ae649f7cbed1401e244bf656c4bc33aa
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2025-07-04 01:44:10 -0300

    chore: add config module

--- backend/src/common/plugins/ws-auth.module.ts
+++ backend/src/common/plugins/ws-auth.module.ts
@@ -1,11 +1,11 @@
-// src/common/plugins/ws-auth.module.ts
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '../../auth/auth.module';
 import { UserModule } from '../../user/user.module';
 import { WebSocketAuthPlugin } from './ws-auth.plugin';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [ConfigModule, AuthModule, UserModule],
   providers: [WebSocketAuthPlugin],
   exports: [WebSocketAuthPlugin],
 })
