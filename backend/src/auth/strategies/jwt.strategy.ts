91f33c81f15c157b5c7f4705792d51d3c32e7ae9
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2025-07-04 01:41:59 -0300

    chore: strategy

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -1,9 +1,9 @@
-// backend/src/auth/jwt.strategy.ts
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt';
 import { User } from 'src/user/user.entity';
+import { UserService } from 'src/user/user.service';
 
 export interface TokenPayload {
   userId: string;
@@ -14,7 +14,10 @@ export interface TokenPayload {
 export class JwtAuthStrategy extends PassportStrategy(JwtStrategy, 'jwt') {
   private readonly logger = new Logger(JwtAuthStrategy.name);
 
-  constructor(private configService: ConfigService) {
+  constructor(
+    private configService: ConfigService,
+    private userService: UserService,
+  ) {
     const jwtSecret = configService.get<string>('JWT_SECRET');
     if (!jwtSecret) {
       throw new Error('JWT_SECRET is not defined in environment variables');
@@ -26,14 +29,20 @@ export class JwtAuthStrategy extends PassportStrategy(JwtStrategy, 'jwt') {
     });
   }
 
-  validate(payload: TokenPayload): User {
+  async validate(payload: TokenPayload): Promise<User> {
     this.logger.debug(
       `Validando access token para payload: ${JSON.stringify(payload)}`,
     );
 
-    return {
-      id: payload.userId,
-      email: payload.email,
-    } as User;
+    const user = await this.userService.findById(payload.userId);
+
+    if (!user) {
+      this.logger.warn(
+        `Usuário com ID ${payload.userId} não encontrado durante validação de JWT.`,
+      );
+      throw new UnauthorizedException('User not found.');
+    }
+
+    return user;
   }
 }
