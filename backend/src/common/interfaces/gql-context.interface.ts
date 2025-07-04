bd1d377ad51f47b638393d2fd9694dbf45c7ce5f
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2025-07-04 01:43:36 -0300

    chore: gql ws context

--- backend/src/common/interfaces/gql-context.interface.ts
+++ backend/src/common/interfaces/gql-context.interface.ts
@@ -1,9 +1,17 @@
-// backend/src/common/interfaces/gql-context.interface.ts
 import { Request, Response } from 'express';
 import { User } from '../../user/user.entity';
+import { WsRequest } from './ws-gql.interface';
 
 export interface GqlContext {
   req: Request & { user?: User };
   res: Response;
   connection?: any;
+}
+
+export interface GqlWsContext {
+  req?: {
+    extra?: {
+      request?: WsRequest;
+    };
+  };
 }
